98195eb4c5831dd581eda5274ae53a6b769bf6be
Jerico <jtsanchez9@up.edu.ph>
2024-05-25 21:31:01 +0800

    feat: added route home screen

--- elbi_donation_system/lib/screens/pendingorg_screen.dart
+++ elbi_donation_system/lib/screens/pendingorg_screen.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'homepage_screen.dart'; 
 
 class PendingOrgScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(""),
+        title: Text("Pending Organization Approval"),
       ),
       body: Center(
-        child: Text(
-          "Pending Application Confirmation",
-          style: TextStyle(fontSize: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Your application is under review.",
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                );
+              },
+              child: Text("Go to Home"),
+            ),
+          ],
         ),
       ),
     );
