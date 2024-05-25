118c2d675161a5a8973a3f61c7790911d3fd1077
Jerico <jtsanchez9@up.edu.ph>
2024-05-25 21:18:29 +0800

    feat: added pending verification screen

---
+++ elbi_donation_system/lib/screens/pendingorg_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PendingOrgScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+      ),
+      body: Center(
+        child: Text(
+          "Pending Application Confirmation",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
